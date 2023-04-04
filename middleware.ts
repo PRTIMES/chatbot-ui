49b33e4035af5d2e847bfcfb69e0cb72eb32f2cd
Iwamoto Yo <you.iwamoto@prtimes.co.jp>
2023-04-04 16:05:23 +0900

    skip basic auth if ip is included by whitelist

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,17 @@ export const config = {
   matcher: ['/'],
 };
 
-// BASIC 認証
+// IP ホワイトリストで検証 → 不正であれば BASIC 認証
 export default async function middleware(req: NextRequest) {
+  // IP ホワイトリスト検証
+  const ipWhitelist = (process.env.IP_WHITELIST as string).split(',');
+  const reqIps = (req.headers.get('x-forwarded-for') as string).split(', ');
+  // IP ホワイトリストに含まれていれば認証スキップ
+  if (reqIps.some((ip) => ipWhitelist.includes(ip))) {
+    return NextResponse.next();
+  }
+
+  // BASIC 認証
   const authorizationHeader = req.headers.get('authorization')?.split(' ')[1];
   if (authorizationHeader) {
     const [user, pass] = atob(authorizationHeader).split(':');
